821b6979d7f10031491bf21d16f551e1d540ff85
sujith8897 <sujithkumar8897@gmail.com>
2021-07-20 21:49:08 +0530

    nav links active

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ $(document).ready(function(){
       $("#icon i").toggleClass("fa-close");
       $('nav ul').toggleClass("show");
     });
+
+    $('nav ul li a').on('click',function(){
+        $('nav ul li a').removeClass('active'); 
+        $(this).addClass('active');
+      });
+      
   });
