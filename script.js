75e38a0f780fa4adbac01f289189e44423b8e83f
sujith8897 <sujithkumar8897@gmail.com>
2021-07-22 22:46:59 +0530

    animation

--- script.js
+++ script.js
@@ -9,5 +9,42 @@ $(document).ready(function(){
         $('nav ul li a').removeClass('active'); 
         $(this).addClass('active');
       });
-      
+
+    $('#tesla').on('click',function(){
+      $('.modal').css('display','block');
+      $('.modal-content').attr('src','images/modely.png');
+    });
+
+    $('#spacex').on('click',function(){
+      $('.modal').css('display','block');
+      $('.modal-content').attr('src','images/spacex.png');
+    });
+
+    $('#boring').on('click',function(){
+      $('.modal').css('display','block');
+      $('.modal-content').attr('src','images/modely.png');
+    });
+
+    $('#neuralink').on('click',function(){
+      $('.modal').css('display','block');
+      $('.modal-content').attr('src','images/modely.png');
+    });
+
+    $('.close').on('click',function(){
+      $('.modal').css('display','none');
+      $('nav ul li a').removeClass('active'); 
+    }); 
+
   });
+
+  function myFunction(thiss) {
+    var id = thiss.id;
+    console.log(id);
+    //document.getElementById('img').style.animation = "";
+    if(id=="tesla"){
+      document.getElementById('img').style.animation = "car 5s linear";
+    } else if(id=="spacex"){
+      document.getElementById('img').style.animation = "rocket 3s linear";
+    }
+    
+  }
